aa5eae16fa03408435cbd33879565567a328c449
Washington Luiz Pedra Amorim <washington.tech@outlook.com.br>
2026-01-24 14:55:30 -0300

    correções

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,10 +5,6 @@ import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
 
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
-
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
@@ -18,7 +14,7 @@ export default function RootLayout() {
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
         <Stack.Screen name='turismo' options={{ headerShown: false }}/>
-        <Stack.Screen name='agendar' options={{ headerShown: false }}/>
+        <Stack.Screen name='agendamento/agendar' options={{ headerShown: false }}/>
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
